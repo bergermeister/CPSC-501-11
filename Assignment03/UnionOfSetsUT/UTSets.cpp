02e214bd0b6b8106118aab0b7eefb7d091ad118c
Edward Eisenberger <eeisenberger3@gmail.com>
2018-10-15 18:48:03 -0400

    Added UnitTest project
    - Added UnionOfSets Unit Test project

---
+++ Assignment03/UnionOfSetsUT/UTSets.cpp
@@ -0,0 +1,19 @@
+#include "CppUnitTest.h"
+#include "..\UnionOfSets\Sets.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnionOfSetsUT
+{		
+	TEST_CLASS( UTSets )
+	{
+	public:
+		TEST_METHOD( MUTUnion )
+		{
+         Sets koSets;
+
+         // TODO: Your test code here
+		}
+
+	};
+}
