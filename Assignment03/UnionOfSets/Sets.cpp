cf074de2c6a090645c052528f19758a86ac7491e
Edward Eisenberger <eeisenberger3@gmail.com>
2018-10-17 17:34:47 -0400

    - Updated MProcess and MUnion

--- Assignment03/UnionOfSets/Sets.cpp
+++ Assignment03/UnionOfSets/Sets.cpp
@@ -1,5 +1,12 @@
 #include "Sets.h"
+#include <algorithm>
+#include <vector>
 #include <set>
+#include <iterator>
+#include <string>
+#include <strstream>
+#include <iomanip>
+#include <cctype>
 
 Sets::Sets( void )
 {
@@ -26,18 +33,97 @@ Sets& Sets::operator=( const Sets& aorSets )
 
 bool Sets::MProcess( std::string aoSet1, std::string aoSet2 )
 {
-   bool kbStatus = false;  // Assume failure
+   using std::string;
+   using std::multiset;
+   using std::vector;
+
+   bool               kbStatus = false;  // Assume failure
+   int                kiIndex;
+   teType             keType[ 2 ]   = { xeTypeInt, xeTypeInt };
+   string             koSetStr[ 2 ] = { aoSet1, aoSet2 };
+   string             koWord;
+   string::iterator   koIter;
+   multiset< int > koWordSet[ 2 ];
+
+   for( kiIndex = 0; kiIndex < 2; kiIndex++ )
+   {
+      koWord = "";
+      for( koIter = koSetStr[ kiIndex ].begin( ); koIter != koSetStr[ kiIndex ].end( ); koIter++ )
+      {
+         if( *koIter == ',' )
+         {
+            break;
+         }
+         else
+         {
+            koWord += *koIter;
+         }
+
+         // If the current type is int a ':' is encountered, switch to time
+         if( keType[ kiIndex ] == xeTypeInt )
+         {
+            if( *koIter == ':' )
+            {
+               // TODO Create time object
+               keType[ kiIndex ] = xeTypeTime;
+            }
+         }
+
+         // If 
+         if( ( ( *koIter < '0' ) || ( *koIter > '9' ) ) && ( *koIter != ' ' ) && ( *koIter != ':' ) )
+         {
+            keType[ kiIndex ] = xeTypeString;
+         }
+      }
+   }
+
+   if( keType[ 0 ] == keType[ 1 ] )
+   {
+      // TODO Create a union
+      switch( keType[ 0 ] )
+      {
+      case xeTypeInt:
+         this->MUnion< int >( koSetStr );
+         break;
+      case xeTypeString:
+         this->MUnion< std::string >( koSetStr );
+         break;
+      case xeTypeTime:
+         // TODO: this->MUnion< 
+         break;
+      }
+   }
+
+   /*
+   vector< int > koResult( 20 );
+   vector< int >::iterator koRIter = koResult.begin( );
+   try
+   {
+      koRIter = std::set_union( koWordSet[ 0 ].begin( ), koWordSet[ 0 ].end( ), 
+                                koWordSet[ 1 ].begin( ), koWordSet[ 1 ].end( ), 
+                                koResult.begin( ) );
+
+      for( koRIter = koResult.begin( ); koRIter != koResult.end( ); koRIter++ )
+      {
+         std::cout << *koRIter << '\t';
+      }
+
+   }
+   catch( std::exception kopException )
+   {
+   }
+   */
+   //koResult = this->MUnion< std::string >( koWordSet[ 0 ], koWordSet[ 1 ], koResult );
 
    // TODO
+
+   //std::for_each( );
 
    return( kbStatus );
 }
 
-std::multiset< int > Sets::MUnion( std::string aoSet1, std::string aoSet2 )
+std::ostream& operator<<( std::ostream& aorOut, const Sets& aorSets )
 {
-   std::multiset< int > koUnion;
-
-   // TODO: This should be a template function or the class should become a template
-
-   return( koUnion );
+   return( aorOut );
 }
+
--- Assignment03/UnionOfSets/Sets.h
+++ Assignment03/UnionOfSets/Sets.h
@@ -3,10 +3,21 @@
 
 #include <set>
 #include <string>
+#include <iostream>
+#include <strstream>
+#include <algorithm>
+#include <iterator>
 
 class Sets
 {
 private:       // Private Attributes
+   enum teType
+   {
+      xeTypeInt    = 0,
+      xeTypeString = 1,
+      xeTypeTime   = 2
+   };
+
    std::multiset< int > aoInts;
    // TODO: Need to implement Time class and multiset of Time Class
 
@@ -17,8 +28,43 @@ public:        // Public Methods
 
    Sets& operator=( const Sets& aorSets );
 
-   bool                 MProcess( std::string aoSet1, std::string aoSet2 );
-   std::multiset< int > MUnion( std::string aoSet1, std::string aoSet2 );
+   bool MProcess( std::string aoSet1, std::string aoSet2 );
+
+   template< class GTcType >
+   std::multiset< GTcType >& MUnion( std::string aoSetStr[ 2 ] )
+   {
+      std::multiset< GTcType > koSet;
+      std::string::iterator    koIter;
+      int                      kiIndex;
+      std::istrstream          koStream0( aoSetStr[ 0 ].c_str( ) );
+      std::istrstream          koStream1( aoSetStr[ 1 ].c_str( ) );
+      GTcType                  koTemp;
+      char                     koComma;
+
+      while( !koStream0.eof( ) )
+      {
+         koStream0 >> koTemp;
+         koStream0 >> koComma;
+         koSet.insert( koTemp );
+      }
+
+      /*
+      for( kiIndex = 0; kiIndex < 2; kiIndex++ )
+      {
+         for( koIter = aoSetStr[ kiIndex ].begin( ); koIter != aoSetStr[ kiIndex ].end( ); koIter++ )
+         {
+            //koSet.insert( *koIter );
+         }
+      }
+      */
+
+      return( koSet );
+   }
+
+   friend std::ostream& operator<<( std::ostream& aorOut, const Sets& aorSets );
+
+private:       // Private Methods
+
 };
 
 #endif
