500139a0ac3467908e107e5354efc0d89498b63c
Edward Eisenberger <eeisenberger3@gmail.com>
2018-11-14 16:23:36 -0500

    Updated DMS class

--- Assignment04/DirectoryManagementSystem/DMS.cpp
+++ Assignment04/DirectoryManagementSystem/DMS.cpp
@@ -6,17 +6,36 @@
 #include "PersonEmailContact.h"
 #include <string>
 #include <map>
+#include <algorithm>
+#include <iostream>
+#include <typeinfo>
 
 DMS::DMS( void )
 {
    voDirectory.insert( std::pair< std::string, Contact* >( "Edward Eisenberger", new PersonPhoneContact  ( "Edward Eisenberger" ) ) );
    voDirectory.insert( std::pair< std::string, Contact* >( "Edward Eisenberger", new PersonAddressContact( "Edward Eisenberger" ) ) );
    voDirectory.insert( std::pair< std::string, Contact* >( "Edward Eisenberger", new PersonEmailContact  ( "Edward Eisenberger" ) ) );
+   voDirectory.insert( std::pair< std::string, Contact* >( "Zainab Al Taweel", new PersonPhoneContact  ( "Zainab Al Taweel" ) ) );
+   voDirectory.insert( std::pair< std::string, Contact* >( "Zainab Al Taweel", new PersonAddressContact( "Zainab Al Taweel" ) ) );
+   voDirectory.insert( std::pair< std::string, Contact* >( "Zainab Al Taweel", new PersonEmailContact  ( "Zainab Al Taweel" ) ) );
+   voDirectory.insert( std::pair< std::string, Contact* >( "Udaya Sree Reddy Teegulla", new PersonPhoneContact  ( "Udaya Sree Reddy Teegulla " ) ) );
+   voDirectory.insert( std::pair< std::string, Contact* >( "Udaya Sree Reddy Teegulla", new PersonAddressContact( "Udaya Sree Reddy Teegulla " ) ) );
+   voDirectory.insert( std::pair< std::string, Contact* >( "Udaya Sree Reddy Teegulla", new PersonEmailContact  ( "Udaya Sree Reddy Teegulla " ) ) );
 
-   for( auto koIter = voDirectory.begin( ); koIter != voDirectory.end( ); koIter++ )
+   std::cout << "Contact Types for Edward Eisenberger: " << std::endl;
+   for( auto koIter = voDirectory.lower_bound( "Edward Eisenberger" ); 
+        koIter != voDirectory.upper_bound( "Edward Eisenberger" ); 
+        koIter++ )
    {
-      koIter->second->display( );
+      std::cout << "Key: " + koIter->first + " \t|\t Type of Value: ";
+      std::cout << typeid( *koIter->second ).name( ) << std::endl;
    }
+}
+
+DMS::DMS( const DMS& aorDMS )
+{
+   // Call assignment operator
+   *this = aorDMS;
 }
 
 DMS::~DMS( void )
@@ -26,4 +45,16 @@ DMS::~DMS( void )
    {
       delete koIter->second;
    }
+}
+
+DMS& DMS::operator=( const DMS& aorDMS )
+{
+   // Prevent self copy
+   if( this != &aorDMS )
+   {
+      // Copy directory
+      this->voDirectory = aorDMS.voDirectory;
+   }
+
+   return( *this );
 }
--- Assignment04/DirectoryManagementSystem/DMS.h
+++ Assignment04/DirectoryManagementSystem/DMS.h
@@ -12,7 +12,14 @@ private:       // Private Attributes
 
 public:        // Public Methods
    DMS( void );
+   DMS( const DMS& aorDMS );
    ~DMS( void );
+
+   DMS& operator=( const DMS& aorDMS );
+
+   // TODO Implement input file read (validate each field)
+   // TODO Implement Search Query
+   // TODO Implement Display Query
 };
 
 #endif
