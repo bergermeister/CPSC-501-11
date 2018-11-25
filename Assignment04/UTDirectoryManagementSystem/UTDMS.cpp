df1c0f88990dbf25f6c67efd3025b1f9bae8ef20
Edward Eisenberger <eeisenberger3@gmail.com>
2018-11-25 17:55:52 -0500

    Implemented Unit Test for Query1

--- Assignment04/UTDirectoryManagementSystem/UTDMS.cpp
+++ Assignment04/UTDirectoryManagementSystem/UTDMS.cpp
@@ -1,17 +1,41 @@
 #include "CppUnitTest.h"
+#include "..\DirectoryManagementSystem\DMS.h"
+#include <vector>
+#include <map>
+#include <string>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
+#define STRINGIFY(x) #x
+#define EXPAND(x) STRINGIFY(x)
+
 namespace UTDirectoryManagementSystem
 {		
-	TEST_CLASS(UnitTest1)
+	TEST_CLASS( UTDMS )
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD( UTQuery1 )
 		{
-			// TODO: Your test code here
+         using namespace std;
+
+         vector< pair< string, int > > koResults;
+			DMS                           koDMS;
+         string                        koDir = EXPAND( PATH );
+         koDir.erase( 0, 1 );                // erase the first quote
+         koDir.erase( koDir.size( ) - 2 );   // erase the last quote and the dot
+
+         koDMS.populateDirectory( koDir + "UTInput1.txt" );
+         koResults = koDMS.mQuery1( "Freya" );
+         
+         Assert::IsTrue( koResults.size( ) == 1,       ( const wchar_t* )"Incorrect Number of Contacts" );
+         Assert::IsTrue( koResults[ 0 ].first == "MD", ( const wchar_t* )"Incorrect State" );
+         Assert::IsTrue( koResults[ 0 ].second == 1,   ( const wchar_t* )"Incorrect Count in State" );
 		}
 
+      TEST_METHOD( UTQuery2 )
+      {
+         
+      }
 	};
 }
