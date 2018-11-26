e1ca63913ecf70927adc7b6fb4cb1ffbecedd776
uteegull <44143282+uteegull@users.noreply.github.com>
2018-11-25 19:21:54 -0500

    ut for query2

--- Assignment04/UTDirectoryManagementSystem/UTDMS.cpp
+++ Assignment04/UTDirectoryManagementSystem/UTDMS.cpp
@@ -33,9 +33,23 @@ namespace UTDirectoryManagementSystem
          Assert::IsTrue( koResults[ 0 ].second == 1,   ( const wchar_t* )"Incorrect Count in State" );
 		}
 
-      TEST_METHOD( UTQuery2 )
+     TEST_METHOD( UTQuery2 )
       {
-         
+		  using namespace std;
+		  vector< pair< string, int > > koResults;
+		  DMS                           koDMS;
+		  string                        koDir = EXPAND(PATH);
+		  koDir.erase(0, 1);                // erase the first quote
+		  koDir.erase(koDir.size() - 2);   // erase the last quote and the dot
+		  koDMS.populateDirectory(koDir + "UTInput1.txt");
+		  koResults = koDMS.mQuery2("edu");
+
+		  Assert::IsTrue(koResults.size() == 1, (const wchar_t*)"Incorrect Number of Contacts");
+		  Assert::IsTrue(koResults[0].first == "Male", (const wchar_t*)"Incorrect Category");
+		  Assert::IsTrue(koResults[0].second == 0, (const wchar_t*)"Incorrect Count of .edu in Male Category");
+		  Assert::IsTrue(koResults[1].first == "Female", (const wchar_t*)"Incorrect Category");
+		  Assert::IsTrue(koResults[0].second == 1, (const wchar_t*)"Incorrect Count of .edu in Female Category");
+
       }
 	};
 }
